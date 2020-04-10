4fc9c11ec2146423fa0707ae571407ad1ac175e8
Winningtopas <jay07@live.nl>
2020-04-10 16:02:30 +0200

    Start

---
+++ app/src/main/java/com/example/shoppinglistkotlin/ProductAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.shoppinglistkotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.move.view.*
+
+class ProductAdapter(private val products: List<Product>) : RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.move, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = products.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(products[position])
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(product: Product) {
+            itemView.tvProduct.text = product.name
+            itemView.tvQuantity.text = product.quantity.toString()
+        }
+    }
+}
